c04bba61160fe03debdd6337c502b6effb7de555
Sakthi <dhamayanthi@flycart.org>
2019-02-01 13:17:20 +0530

    - Version update

--- retainful.php
+++ retainful.php
@@ -5,7 +5,7 @@
  * Description: Drive repeat purchases by sending single-use, unique coupon codes to customers for their next purchase
  * Author: Retainful
  * Author URI: https://www.retainful.com
- * Version: 1.0.3
+ * Version: 1.0.4
  * Slug: retainful-next-order-coupon-for-woocommerce
  * Text Domain: retainful-woocommerce
  * Domain Path: /i18n/languages/
@@ -43,7 +43,7 @@ if (!defined('RNOC_TEXT_DOMAIN'))
  * Current version of our app
  */
 if (!defined('RNOC_VERSION'))
-    define('RNOC_VERSION', '1.0.3');
+    define('RNOC_VERSION', '1.0.4');
 
 /**
  * Set base file URL
