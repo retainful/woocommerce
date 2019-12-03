4644d54e6333a2073fd525145faf0f578cf6004f
retainful <iamsathyaseelan@gmail.com>
2019-12-03 17:15:58 +0530

    - v2-development conditional field added

---
+++ src/fields/cmb-field-conditional-fields/cmb-field-conditional-fields.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * CMB2 Conditionals.
+ */
+if (!class_exists('CMB2_Conditionals', false)) {
+    /**
+     * CMB2_Conditionals Plugin.
+     */
+    class CMB2_Conditionals
+    {
+        /**
+         * Constructor - Set up the actions for the plugin.
+         */
+        public function __construct()
+        {
+            add_action('admin_footer', array($this, 'setUpAdminScripts'));
+        }
+
+        /**
+         * Setup conditional fields
+         */
+        function setUpAdminScripts()
+        {
+            $script_src = apply_filters('cmb2_conditionals_enqueue_script_src', plugins_url('/cmb-field-conditional-fields.js', __FILE__));
+            wp_enqueue_script('cmb2-conditionals', $script_src, array('jquery', 'cmb2-scripts'), RNOC_VERSION, true);
+        }
+    }
+} /* End of class-exists wrapper. */
+new CMB2_Conditionals();
