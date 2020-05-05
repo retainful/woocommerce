ff693bb1150c765d1ec24c1c693d96c9ca73b618
Sathyaseelan <sathyaseelan@cartrabbit.in>
2020-05-05 17:44:52 +0530

    -(Fix) validating NOC coupon before saving added

--- src/admin/js/app.js
+++ src/admin/js/app.js
@@ -1,5 +1,13 @@
 (function ($) {
     $(document).ready(function () {
+        $(document).on("click", "#rnoc_retainful #submit-cmb", function (event) {
+            var is_noc_explained = $('[name="rnoc_enable_next_order_coupon"]:checked').val();
+            var noc_coupon_val = $('[name="rnoc_retainful_coupon_amount"]').val();
+            if (parseInt(is_noc_explained) === 1 && (parseInt(noc_coupon_val) <= 0 || noc_coupon_val === "")) {
+                alert("Coupon value must not empty and greater than 0!");
+                return false;
+            }
+        })
         $(document).on("keypress keyup blur", "#app_coupon_value", function (event) {
             $(this).val($(this).val().replace(/[^0-9\.]/g, ''));
             if ((event.which !== 46 || $(this).val().indexOf('.') !== -1) && (event.which < 48 || event.which > 57)) {
