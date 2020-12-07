3e7add32b2908b28040fab5a8cbbdc4a32d7e989
Sathyaseelan <sathyaseelan@cartrabbit.in>
2020-12-07 12:51:15 +0530

    - Basic combine

--- src/premium/addons/RetainfulAddToCartAddon.php
+++ src/premium/addons/RetainfulAddToCartAddon.php
@@ -403,6 +403,7 @@ if (!class_exists('RetainfulAddToCartAddon')) {
          */
         function addSiteScripts()
         {
+
             if (!wp_script_is('rnoc-add-to-cart')) {
                 wp_enqueue_script('rnoc-add-to-cart', RNOCPREMIUM_PLUGIN_URL . 'assets/js/atc-popup.min.js', array('wc-add-to-cart', 'wc-add-to-cart-variation'), RNOC_VERSION);
             }
