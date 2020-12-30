bb02b46148cbea82300883171a46a606ed8a698d
Sathyaseelan <sathyaseelan@cartrabbit.in>
2020-12-30 15:58:14 +0530

    -(FIX) Missing ID attribute

--- src/templates/pro-addons.php
+++ src/templates/pro-addons.php
@@ -7,7 +7,7 @@
     !function (e, c) {
         !function (e) {
             const o = c.createElement("script");
-            o.type = "application/javascript", o.src = e,o.setAttribute("data-app_id", "<?php echo $params['api_key'] ?>"),o.setAttribute("data-cfasync", "false"),o.setAttribute("data-customer-first-name", "<?php echo esc_attr__($params['first_name']); ?>"),o.setAttribute("data-customer-id", "<?php echo $params['user_id'] ?>"),o.setAttribute("data-customer-email", "<?php echo esc_attr__($params['email']) ?>"), c.body.appendChild(o)
+            o.type = "application/javascript", o.src = e,o.setAttribute("data-app_id", "<?php echo $params['api_key'] ?>"),o.setAttribute("id", "retainful-popups-script-js"),o.setAttribute("data-cfasync", "false"),o.setAttribute("data-customer-first-name", "<?php echo esc_attr__($params['first_name']); ?>"),o.setAttribute("data-customer-id", "<?php echo $params['user_id'] ?>"),o.setAttribute("data-customer-email", "<?php echo esc_attr__($params['email']) ?>"), c.body.appendChild(o)
         }("<?php echo $params['pro_popup_url'] ?>")
     }(window, document);
 </script>
