eb42e59cd9cb9c82ac88603178b3c5cc20f5926c
Alagesan <alagesanbe08@gmail.com>
2023-07-31 15:12:15 +0530

    - HPOS support

--- retainful-next-order-coupon-for-woocommerce.php
+++ retainful-next-order-coupon-for-woocommerce.php
@@ -116,6 +116,11 @@ if (!function_exists('rnocPluginActivation')) {
         return true;
     }
 }
+add_action('before_woocommerce_init', function () {
+    if (class_exists(\Automattic\WooCommerce\Utilities\FeaturesUtil::class)) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+    }
+});
 /**
  * Check the woocommerce is active or not
  * @return bool
