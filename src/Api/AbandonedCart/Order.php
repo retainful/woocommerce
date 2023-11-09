e3c89a58e1c3af469b1bdc37a0ed88287e84def5
Alagesan <alagesanbe08@gmail.com>
2023-11-09 14:59:11 +0530

    - order payment method

--- src/Api/AbandonedCart/Order.php
+++ src/Api/AbandonedCart/Order.php
@@ -260,7 +260,8 @@ class Order extends RestApi
             'recovered_cart_token' => self::$woocommerce->getOrderMeta($order, '_rnoc_recovered_cart_token'),
             'recovered_at' => (!empty($recovered_at)) ? $this->formatToIso8601($recovered_at) : NULL,
             'noc_discount_codes' => $this->getNextOrderCouponDetails($order),
-            'client_details' => $this->getClientDetails($order)
+            'client_details' => $this->getClientDetails($order),
+            'payment_method' => $order->get_payment_method(),
         );
         if(!empty($cart_token)){
             $referrer_automation_id = self::$woocommerce->getSession($cart_token.'_referrer_automation_id');
