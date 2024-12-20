8d27e9fa7c9e047e61da340b92174f1804af8b2a
Alagesan <alagesanbe08@gmail.com>
2024-12-20 15:13:57 +0530

    - version 2.6.39

--- retainful-next-order-coupon-for-woocommerce.php
+++ retainful-next-order-coupon-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Do life-cycle WooCommerce email marketing with Retainful - grow email list, nurture and retain customers through automation & email campaigns.
  * Author: Retainful
  * Author URI: https://www.retainful.com
- * Version: 2.6.38
+ * Version: 2.6.39
  * Slug: retainful-next-order-coupon-for-woocommerce
  * Text Domain: retainful-next-order-coupon-for-woocommerce
  * Domain Path: /i18n/languages/
@@ -13,7 +13,7 @@
  * Requires at least: 4.7.0
  * Contributers: Sathyaseelan
  * WC requires at least: 6.0.0
- * WC tested up to: 9.3
+ * WC tested up to: 9.5
  */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -34,7 +34,7 @@ if ( ! defined( 'RNOC_PLUGIN_SLUG' ) ) {
  * Current version of our app
  */
 if ( ! defined( 'RNOC_VERSION' ) ) {
-	define( 'RNOC_VERSION', '2.6.38' );
+	define( 'RNOC_VERSION', '2.6.39' );
 }
 /**
  * Set base file URL
@@ -213,8 +213,8 @@ if ( ! function_exists( 'rnocEscAttr' ) ) {
 /**
  * check is woocommerce is active
  */
-if ( ! rnocIsWoocommerceActive()  && !function_exists( 'WC' ) ) {
-    return '';
+if ( ! rnocIsWoocommerceActive() && ! function_exists( 'WC' ) ) {
+	return '';
 }
 require __DIR__ . '/vendor/autoload.php';
 
