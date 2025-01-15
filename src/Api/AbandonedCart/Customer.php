58bcf3b07e0b2bb4ba862a1769592b4f5965067f
thomas <thomastomy2001@gmail.com>
2025-01-15 13:45:04 +0530

    - add sanitize and email validation

--- src/Api/AbandonedCart/Customer.php
+++ src/Api/AbandonedCart/Customer.php
@@ -4,6 +4,8 @@
 namespace Rnoc\Retainful\Api\AbandonedCart;
 
 
+use function WPML\FP\apply;
+
 class Customer {
 
 	/**
@@ -17,13 +19,22 @@ class Customer {
 		if(empty($request)) {
 			 return;
 		 }
-		 $order_arg = [
-			 'email' => !empty( $request->get_param('email')) ? $request->get_param('email') : '',
-			 'date_after' => !empty( $request->get_param('date_after')) ?  $request->get_param('date_after') : '',
-			 'post_status' => ['wc-completed', 'wc-processing'],
-		 ];
+		$email = !empty( $request->get_param('email')) ? sanitize_email( $request->get_param('email') ): '';
+		$after_date = !empty( $request->get_param('date_after')) ?  sanitize_text_field( $request->get_param('date_after') ) : '';
+
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			return new \WP_Error('invalid_email', 'The email address provided is invalid.', ['status' => 400]);
+		}
+
+		$order_arg = [
+			 'email' =>  $email,
+			 'date_after' => $after_date,
+			 'post_status' => apply_filters('rnoc_customer_order_sync_order_status',['wc-completed', 'wc-processing']),
+		];
+
 		$orders = function_exists('wc_get_orders' ) && !empty($order_arg) ?  wc_get_orders($order_arg) : '';
-		if(!empty($orders)) {
+
+		if(!empty($orders) && is_array($orders) && count($orders) > 0) {
 			$response = [
 				'success'       => true,
 			];
