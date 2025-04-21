059a9d8b8da25ec0c3615daeae98dc69b15288e3
thomas <thomas@cartrabbit.in>
2025-04-21 12:01:06 +0530

    - fixed plugin check issue in below_discount.php file

--- src/premium/templates/timer/below_discount.php
+++ src/premium/templates/timer/below_discount.php
@@ -1,6 +1,6 @@
-<div class="rnoc-coupon-timer-container-<?php echo $coupon_timer_position ?>"><?php echo $rnoc_coupon_timer_message; ?></div>
+<div class="rnoc-coupon-timer-container-<?php echo esc_attr($coupon_timer_position) ?>"><?php echo esc_html($rnoc_coupon_timer_message); ?></div>
 <script>
-    var timerStarted = parseInt(<?php echo $is_timer_started; ?>);
+    var timerStarted = parseInt(<?php echo esc_js($is_timer_started); ?>);
     <?php
     if ($is_timer_reset == 1) {
     ?>
@@ -18,7 +18,7 @@
     } else {
         startedTime = parseInt(timerStartTime);
     }
-    var endedTimeInMin = parseInt("<?php echo $expired_in_min; ?>") * 60000;
+    var endedTimeInMin = parseInt("<?php echo esc_js($expired_in_min); ?>") * 60000;
     var countDownDate = startedTime + endedTimeInMin;
     window.rnoc_timer_expired_message_shown = false;
     var x = setInterval(function () {
@@ -28,15 +28,15 @@
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("rnoc-coupon-timer-<?php echo $coupon_timer_position ?>").innerHTML = <?php echo $rnoc_coupon_timer_display_format ?>;
+        document.getElementById("rnoc-coupon-timer-<?php echo esc_js($coupon_timer_position) ?>").innerHTML = <?php echo esc_js($rnoc_coupon_timer_display_format) ?>;
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("rnoc-coupon-timer-<?php echo $coupon_timer_position ?>").innerHTML = "<?php echo __($coupon_timer_expired_text, RNOC_TEXT_DOMAIN) ?>";
+            document.getElementById("rnoc-coupon-timer-<?php echo esc_js($coupon_timer_position) ?>").innerHTML = "<?php echo esc_html__($coupon_timer_expired_text, 'retainful-next-order-coupon-for-woocommerce') //phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralText	 ?>";
             <?php
             if(apply_filters('rnoc_coupon_timer_top_position_reload', true)){
             if($auto_fix_page_reload == 0){
             ?>
-            jQuery.post("<?php echo admin_url('admin-ajax.php?action=rnoc_coupon_timer_expired') ?>", function (data, status) {
+            jQuery.post("<?php echo esc_url(admin_url('admin-ajax.php?action=rnoc_coupon_timer_expired')) ?>", function (data, status) {
                 if (data.success) {
                     window.location.reload();
                 }
@@ -45,13 +45,13 @@
             }else {
             ?>
             (function ($) {
-                $.post("<?php echo admin_url('admin-ajax.php?action=rnoc_coupon_timer_expired') ?>", function (data, status) {
+                $.post("<?php echo esc_url(admin_url('admin-ajax.php?action=rnoc_coupon_timer_expired')) ?>", function (data, status) {
                     if (data.success) {
                         if (window.rnoc_timer_expired_message_shown === false) {
-                            var coupon_det = $('.coupon-<?php echo strtolower($coupon_code); ?>');
+                            var coupon_det = $('.coupon-<?php echo esc_attr(strtolower($coupon_code)); ?>');
                             coupon_det.hide();
                             var wrapper = $(".woocommerce-notices-wrapper");
-                            var html = '<ul class="woocommerce-error" role="alert"><li><?php echo $coupon_timer_expire_message; ?></li></ul>';
+                            var html = '<ul class="woocommerce-error" role="alert"><li><?php echo esc_js($coupon_timer_expire_message); ?></li></ul>';
                             wrapper.append(html);
                             window.rnoc_timer_expired_message_shown = true;
                         }
@@ -66,24 +66,24 @@
     }, 1000);
 </script>
 <style>
-    .rnoc-coupon-timer-container-<?php echo $coupon_timer_position ?> {
+    .rnoc-coupon-timer-container-<?php echo esc_attr($coupon_timer_position) ?> {
         padding: 20px 5px;
         text-align: center;
-        background: <?php echo $rnoc_coupon_timer_background; ?> !important;;
+        background: <?php echo esc_attr($rnoc_coupon_timer_background); ?> !important;;
         width: 100%;
         right: 0;
         margin: 10px 0;
         font-size: 16px;
-        color: <?php echo $rnoc_coupon_timer_color ?>;
+        color: <?php echo esc_attr($rnoc_coupon_timer_color) ?>;
     }
 
-    .rnoc-coupon-timer-container-<?php echo $coupon_timer_position ?> .timer-coupon-code-<?php echo $coupon_timer_position ?> {
+    .rnoc-coupon-timer-container-<?php echo esc_attr($coupon_timer_position) ?> .timer-coupon-code-<?php echo esc_attr($coupon_timer_position) ?> {
         font-weight: 800;
-        color: <?php echo $rnoc_coupon_timer_coupon_code_color; ?> !important;
+        color: <?php echo esc_attr($rnoc_coupon_timer_coupon_code_color); ?> !important;
     }
 
-    .rnoc-coupon-timer-container-<?php echo $coupon_timer_position ?> #rnoc-coupon-timer-<?php echo $coupon_timer_position ?> {
+    .rnoc-coupon-timer-container-<?php echo esc_attr($coupon_timer_position) ?> #rnoc-coupon-timer-<?php echo esc_attr($coupon_timer_position) ?> {
         font-weight: 800;
-        color: <?php echo $rnoc_coupon_timer_coupon_timer_color; ?> !important;
+        color: <?php echo esc_attr($rnoc_coupon_timer_coupon_timer_color); ?> !important;
     }
 </style>
