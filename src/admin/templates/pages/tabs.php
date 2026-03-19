85790454023b35b83ae528763c11249646d519cb
Naveen-kumar-1 <codemavericknk@gmail.com>
2026-03-19 15:53:32 +0530

    Danger banner css added

--- src/admin/templates/pages/tabs.php
+++ src/admin/templates/pages/tabs.php
@@ -1,9 +1,10 @@
 <?php
 $page = isset($_GET['page']) ? sanitize_text_field(wp_unslash($_GET['page'])) : NULL; //phpcs:ignore WordPress.Security.NonceVerification.Recommended
 ?>
-<div style="background-color: #d63638; color: white; padding: 10px; margin-bottom: 20px;border-radius: 8px;">
-    <h1 style="color: white;">Retainful V3 — Migration Required by April 15th</h1>
-    <p style="font-size: 17px;">We've rebuilt Retainful from the ground up with a new secure WooCommerce REST API. Your store needs to be reconnected on the new platform to continue sending emails. All your contacts, lists, and flows carry over. Migrate now — it takes a few minutes.<a href="https://app.retainful.com/" class="button button-primary align-center" target="_blank">Migrate My Account</a></p>
+
+<div class="rnoc-notice-container">
+    <h1>Retainful V3 — Migration Required by April 15th</h1>
+    <p>We've rebuilt Retainful from the ground up with a new secure WooCommerce REST API. Your store needs to be reconnected on the new platform to continue sending emails. All your contacts, lists, and flows carry over. Migrate now — it takes a few minutes.<a href="https://app.retainful.com/" target="_blank">Migrate My Account</a></p>
 </div>
 <h2 class="nav-tab-wrapper">
     <a class="nav-tab <?php if ($page == 'retainful_license') {
